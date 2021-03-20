09a7b98087be2f26701ef0c149e2d1c2760a8c5d
Abhay Zala <zala.abhay@gmail.com>
2021-03-20 14:37:20 -0400

    Page Signup

---
+++ front-end/firebase/signup.js
@@ -0,0 +1,44 @@
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	$("#signup-error").html("");
+
+	const email = signupForm['email'].value;
+	const password = signupForm['password'].value;
+	const confirmPassword = signupForm['confirm-password'].value;
+
+	if (password !== confirmPassword) {
+		$("#signup-error").html("Passwords do not match");
+	} else {
+		createUser(email, password);
+	}
+});
+
+function createUser(email, password) {
+	if (auth.currentUser != null) {
+		auth.signOut().then(() => {
+			createUser(email, password);
+		});
+	} else {
+		auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+			auth.currentUser.sendEmailVerification().then(() => {
+				db.collection("users").doc(auth.currentUser.uid).set({
+					uid: auth.currentUser.uid,
+					email: email,
+					profilePic: 'default.png',
+					tags: [],
+					friends: []
+				}).then(() => {
+					auth.signOut().then(() => {
+						console.log("sent");
+						drawCheck();
+					});
+				});
+			});
+
+		}).catch(err => {
+			$("#signup-error").html(err.message);
+		});;
+	}
+}
