fbb6ec4c1744a8febb23e8b91c0300325e0c0a0a
Abhay Zala <zala.abhay@gmail.com>
2021-03-20 12:40:11 -0400

    Removed test code and set port to 3000

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-/*const fs = require('fs');
+const fs = require('fs');
 const https = require('https');
 const bodyParser = require('body-parser');
 
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3001;
+const port = 3000;
 
 const httpsServer = https.createServer({
 	key: fs.readFileSync('/etc/letsencrypt/live/api.aszala.com/privkey.pem'),
@@ -29,15 +29,3 @@ httpsServer.listen(port, () => {
 app.get('/', (req, res) => {
 	res.send("Hello World");
 });
-*/
-
-const appTest = express()
-const portTest = 3000
-
-appTest.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-appTest.listen(portTest, () => {
-  console.log(`Example app listening at http://localhost:${portTest}`)
-})
