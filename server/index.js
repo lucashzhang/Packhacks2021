138344304b4cd2538dd2018b85fb8e0d26987fb2
Abhay Zala <zala.abhay@gmail.com>
2021-03-20 12:30:08 -0400

    Bug fix and reading tags from intents

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const fs = require('fs');
 const https = require('https');
 const bodyParser = require('body-parser');
 
+let intents_data = JSON.parse(fs.readFileSync('./chat-model/intents.json'));
+let self_tags = [];
+
+for (let i=0;i<intents_data.intents.length;i++) {
+	self_tags.push(intents_data.intents[i].tag);
+}
+
 const app = express();
-app.use(expressip().getIpInfoMiddleware);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
