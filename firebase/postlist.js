eb7d4a22ea7272ff35968eadcb96e28b537936f0
Abhay Zala <zala.abhay@gmail.com>
2021-03-21 03:05:30 -0400

    Fixed post list css

--- firebase/postlist.js
+++ firebase/postlist.js
@@ -24,20 +24,25 @@ function displayAllPosts() {
     document.querySelectorAll('.postListContainer').forEach(e => e.remove());
     let posts = db.collection('posts');
     posts.get().then((querySnapshot) => {
-        let post_div = document.createElement("div");
-        querySnapshot.forEach((doc) => {        
+        let post_div = $("#post-container");
+        querySnapshot.forEach((doc) => {
             if (doc.id !== "postcount") {
                 let this_thread = doc.data().alldata;
                 let user = this_thread[0];
                 let post = this_thread[1];
                 let likes = this_thread[2];
-                post_div.innerHTML = post_div.innerHTML + "<div class='postListContainer'><a href=post.html?post_ID=" +  doc.id +  " id='postList'>" + " [" + likes + " likes] "  + user + ": "+ post  +"</a></div>"
-                console.log(doc.id, " => ", doc.data());
+                //post_div.innerHTML = post_div.innerHTML + "<div class='postListContainer'><a href=post.html?post_ID=" +  doc.id +  " id='postList'>" + " [" + likes + " likes] "  + user + ": "+ post  +"</a></div>"
+				post_div.append(`<a class='postListContainer' href='post.html?post_ID=${doc.id}'>
+									<div>
+										<div class="post-title">${post}</div>
+										<div class="post-author">Asked by ${user}</div>
+										<div class="likes">${likes}<span class="heart">&hearts;</span></div>
+									</div>
+								</a><hr>`);
             }
-            
-                
+
+
         });
-        document.body.appendChild(post_div)
 });
 }
 
@@ -56,11 +61,11 @@ function writeUserData() {
             post.set({
                 user_id: this_user_id,
                 alldata: [this_user, document.getElementById('addCommentTxt').value, 0],
-                
+
             })
             displayAllPosts()
-        } 
-        
+        }
+
     })
-    
+
   }
