c6e912bfb3ed4289ba53441cb614c57d6efc84cf
Abhay Zala <zala.abhay@gmail.com>
2021-03-20 23:46:30 -0400

    Test

--- server/index.js
+++ server/index.js
@@ -88,6 +88,7 @@ app.get('/parse_img', async (req, res) => {
 	fs.readFile('./ocr-convert-image-to-text/out/' + req.query.filename + ".txt", 'utf8', function(err, data) {
 		if (err) throw err;
 		let lines = data.split("\n");
+		console.log(data);
 		res.send(lines.join(', '));
 	});
 });
