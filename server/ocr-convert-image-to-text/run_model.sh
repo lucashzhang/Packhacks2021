ea04b5362d9b5028844c1a7a411125c236ea5ea4
Abhay Zala <zala.abhay@gmail.com>
2021-03-20 23:01:50 -0400

    Pipe Test

--- server/ocr-convert-image-to-text/run_model.sh
+++ server/ocr-convert-image-to-text/run_model.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo python ./ocr-convert-image-to-text/main.py -i ./ocr-convert-image-to-text/inputs/ -o ./ocr-convert-image-to-text/out/
+echo | python ./ocr-convert-image-to-text/main.py -i ./ocr-convert-image-to-text/inputs/ -o ./ocr-convert-image-to-text/out/
