65dd5fbce7526e32aa21a6e3842decd55051d22f
Abhay Zala <zala.abhay@gmail.com>
2021-03-20 21:28:28 -0400

    Send form to server

--- front-end/chat.js
+++ front-end/chat.js
@@ -1,35 +1,12 @@
-$("#image-upload").on("change", function(e) {
-	var file = $(this)[0].files[0];
-	var data = new FormData();
-
-	data.append('file', file);
+$("#upload-image-form").submit(function(e) {
+	e.preventDefault();
 
 	$.ajax({
-		url: "https://aszala.com:3000/upload_img",
+		url: "https://api.aszala.com:3000/upload_img",
 		type: 'post',
-		data: data,
-		contentType: false,
-		processData: false,
-		success: function (response) {
-			console.log(response);
-		},
-		error: function (res) {
-			console.log(res);
+		data: $("#upload-image-form").serialize(),
+		success: function() {
+			console.log("dawjjdwkljkl");
 		}
 	});
-/*
-	that = this;
-	formData = new FormData();
-	formData.append("filename", file.name);
-
-	$.ajax({ type: "POST", url: "https://aszala.com:3000/prase_img", xhr: function () {
-		var myXhr = $.ajaxSettings.xhr();
-
-		return myXhr;
-	}, success: function (data) {
-		console.log(data);
-	}, error: function (error) {
-
-	},
-	async: true, data: formData, cache: false, contentType: false, processData: false, timeout: 60000 });*/
 });
